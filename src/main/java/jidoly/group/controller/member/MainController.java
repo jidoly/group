5be07f4fe35e7c0519ee0b29382132c5be3d36a1
jidoly <jidol8849@gmail.com>
2023-12-20 23:41:53 +0900

    feat : [temp]ver 0.41 minor - make main page

--- src/main/java/jidoly/group/controller/member/MainController.java
+++ src/main/java/jidoly/group/controller/member/MainController.java
@@ -20,4 +20,24 @@ public class MainController {
     public String main() {
         return "main";
     }
+    @GetMapping("/main2")
+    public String main2() {
+        return "main2";
+    }
+    @GetMapping("/main3")
+    public String main3() {
+        return "main3";
+    }
+    @GetMapping("/main4")
+    public String main4() {
+        return "main4";
+    }
+    @GetMapping("/main5")
+    public String main5() {
+        return "main5";
+    }
+    @GetMapping("/temp")
+    public String temp() {
+        return "makemain";
+    }
 }
